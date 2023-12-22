52c0411c0e0f15f194f7818daa5f3f3bcb4141e0
Hiru <waruna.fernando@gmail.com>
2023-12-22 01:06:04 -0800

    compiler_depend

---
+++ humble/learning/build/articubot_one/CMakeFiles/articubot_one_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/articubot_one_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/articubot_one_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ humble/learning/build/articubot_one/CMakeFiles/articubot_one_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for articubot_one_uninstall.
+# This may be replaced when dependencies are built.
